208192302a3be7955f616cbe403b6843527eba2e
Александр <gonchar-aleksandr-avich@yandex.ru>
2023-04-08 20:30:58 +0300

    добавил вывод даты профессионального праздника

---
+++ Praktikum.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+    public class Praktikum {
+
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите год:");
+            int year = scanner.nextInt();
+            String strDate;
+
+            if (isLeapYear(year)) {
+                System.out.println(year + "-й год - високосный, будте осторожны!!!");
+                strDate="12.09.";
+            } else {
+                System.out.println(year + "-й год - НЕ високосный, живите спокойно!!!");
+                strDate= "13.09.";
+            }
+            isPrintDate(strDate,year);
+        }
+
+        public static boolean isLeapYear(int year) {
+            // здесь нужно определить, является ли переданный год високосным
+            if (year % 4==0) {
+                if (year%100==0) {
+                    if (year % 400 !=0)
+                        return true;
+                    else
+                        return false;
+                }
+                else
+                    return true;
+            }
+            return false;
+        }
+
+        public static void isPrintDate(String strDate, int inYear) {
+            System.out.println("В этом году профессиональный праздник будет: "+ strDate+inYear);
+        }
+    }
+
